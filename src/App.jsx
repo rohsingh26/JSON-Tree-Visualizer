84a944c98c5c0d8eb782b311fc4b15e0c039c97f
Rohit Kumar Singh <rohitsingh60881@gmail.com>
2025-10-28 03:53:22 +0530

    Initialized the repo and completed the setup of react app with scss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>JSON Tree Visualizer</h1>
+      <p>Initialized the app with react and scss</p>
+      <button>Let's Built</button>
+    </div>
+  );
+}
+
+export default App;
